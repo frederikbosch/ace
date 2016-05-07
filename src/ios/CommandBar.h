68be6d746cebc2cb0c477ce2b7b338732f5a93c4
Frederik Bosch <f.bosch@genkgo.nl>
2016-05-07 19:08:38 +0200

    Update CommandBar.h

--- src/ios/CommandBar.h
+++ src/ios/CommandBar.h
@@ -3,9 +3,10 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 #import "IHaveProperties.h"
+#import "IRecieveCollectionChanges.h"
 #import "CommandBarElementCollection.h"
 
-@interface CommandBar : NSObject <IHaveProperties>
+@interface CommandBar : NSObject <IHaveProperties, IRecieveCollectionChanges>
 {
     CommandBarElementCollection* _primaryCommands;
     CommandBarElementCollection* _secondaryCommands;
